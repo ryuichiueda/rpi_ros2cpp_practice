063f80f2770fddda13d2cbbab1d729d9f68e0f50
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-07-06 13:53:55 +0900

    Add a timer

--- src/lightsensors.cpp
+++ src/lightsensors.cpp
@@ -1,5 +1,8 @@
+#include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int16_multi_array.hpp"
+
+using namespace std::chrono_literals;
 
 class Lightsensors : public rclcpp::Node
 {
@@ -7,10 +10,18 @@ public:
     Lightsensors() : Node("lightsensors")
     {
         publisher_ = this->create_publisher<std_msgs::msg::Int16MultiArray>("lightsensors", 10);
+	timer_ = this->create_wall_timer(100ms, std::bind(&Lightsensors::timer_callback, this));
     }
 
 private:
+    void timer_callback()
+    {
+        auto message = std_msgs::msg::Int16MultiArray();
+        publisher_->publish(message);
+    }
+
     rclcpp::Publisher<std_msgs::msg::Int16MultiArray>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
 };
 
 int main(int argc, char* argv[])
